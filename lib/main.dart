9f754707b78e239b7cde7e6b6d33ea71f919f260
Troy Dwijanto <troy.dwijanto2@gmail.com>
2022-09-03 17:10:04 -0500

    Dropped native-view and laying groundwork for VISL intepreter integration
    - All platforms will now use Material3 (for now)
    - Working on VISL bindings

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,29 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart' as material;
+import 'package:flutter/material.dart';
 import 'package:osab/material/home.dart';
-import 'package:osab/meta/navigation.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future<void> main(List<String> args) async {
-  material.WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
     sqfliteFfiInit();
   }
-  switch (defaultTargetPlatform) {
-    case (TargetPlatform.iOS):
-      material.runApp(const AndroidApp());
-      break;
-    case (TargetPlatform.windows):
-      material.runApp(const AndroidApp());
-      break;
-    case (TargetPlatform.linux):
-      material.runApp(const AndroidApp());
-      break;
-    case (TargetPlatform.fuchsia):
-    case (TargetPlatform.android):
-    default:
-      material.runApp(const AndroidApp());
-      break;
-  }
+  runApp(const AndroidApp());
 }
 
-class AndroidApp extends material.StatefulWidget {
+class AndroidApp extends StatelessWidget {
   const AndroidApp({super.key});
   @override
-  material.State<material.StatefulWidget> createState() {
-    return _AndroidApp();
-  }
-}
-
-class _AndroidApp extends material.State<AndroidApp> {
-  @override
-  material.Widget build(material.BuildContext context) {
-    return material.MaterialApp(
-      navigatorKey: GlobalNavigation.globalNavigation,
-      theme:
-          material.ThemeData(useMaterial3: true, brightness: Brightness.light),
-      darkTheme:
-          material.ThemeData(useMaterial3: true, brightness: Brightness.dark),
-      themeMode: material.ThemeMode.system,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true, brightness: Brightness.light),
+      darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: const material.Scaffold(body: AndroidHome()),
+      title: "Watercraft Interface Program - Flutter",
+      home: const Scaffold(body: AndroidHome()),
     );
   }
 }
--- lib/meta/navigation.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GlobalNavigation {
-  static GlobalKey<NavigatorState> globalNavigation =
-      GlobalKey<NavigatorState>();
-}
